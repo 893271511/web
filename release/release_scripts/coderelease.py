e91032f807b990a40058510c6f9131d7a63e5315
893271511 <893271511@qq.com>
2016-04-12 15:13:23 +0800

    python重写发布脚本

---
+++ release/release_scripts/coderelease.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+#coding:utf-8
+#脚本使用例：python coderelease.py renren-licai-credit-manager 31614 test 10.2.54.240
+import sys,os
+print("脚本名：" + sys.argv[0])
+print("项目名：" + sys.argv[1])
+print("版本号：" + sys.argv[2])
+print("环  境：" + sys.argv[3])
+print("服务器：" + sys.argv[4])
+
+#设置环境
+def set_env():
+    #kerberos
+    os.system('export PATH="/usr/kerberos/bin:$PATH"')
+    os.system('export KRB5CCNAME=/tmp/krb5cc_pub_$$')
+    os.system('trap kdestroy 0 1 2 3 5 15')
+    os.system('kinit -k -t /etc/krb5.keytab')
+    #lanage
+    os.system('export LANG="en_US.UTF-8"')
+    os.system('export LC_ALL="en_US.utf8"')
+    os.system('export LC_LANG="en_US.utf8"')
+    #java_home
+    os.system('export JAVA_HOME=/usr/java/jdk')
+    os.system('export CLASS_PATH=.:$JAVA_HOME/lib')
+    os.system('export PATH=$JAVA_HOME/bin:$PATH')
+    #maven
+    os.system('export M2_HOME=/usr/local/maven-2.1.0')
+    os.system('export M2=$M2_HOME/bin')
+    os.system('export MAVEN_OPTS=-Dfile.encoding=utf-8')
+    os.system('export PATH=$PATH:$M2')
+    
+def check_env():
+    pass
