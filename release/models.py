03f87495436aa12be6a53eae2e4ebd0422aa74c3
893271511 <893271511@qq.com>
2016-07-07 18:10:06 +0800

    增加权限管理功能

--- release/models.py
+++ release/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import *
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
@@ -28,3 +29,29 @@ class Project(models.Model):
     def __str__(self):
         return self.name
 
+
+# class UserGroup(models.Model):
+#     name = models.CharField(max_length=80, unique=True)
+#     comment = models.CharField(max_length=160, blank=True, null=True)
+#
+#     def __unicode__(self):
+#         return self.name
+
+# class User(AbstractUser):
+#     # USER_ROLE_CHOICES = (
+#     #     ('SU', 'SuperUser'),
+#     #     ('GA', 'GroupAdmin'),
+#     #     ('CU', 'CommonUser'),
+#     # )
+#     name = models.CharField(max_length=80)
+#     # uuid = models.CharField(max_length=100)
+#     # role = models.CharField(max_length=2, choices=USER_ROLE_CHOICES, default='CU')
+#     # group = models.ManyToManyField(UserGroup)
+#     # ssh_key_pwd = models.CharField(max_length=200)
+#     # # is_active = models.BooleanField(default=True)
+#     # # last_login = models.DateTimeField(null=True)
+#     # # date_joined = models.DateTimeField(null=True)
+#     #
+#     # def __unicode__(self):
+#     #     return self.username
+#     #
